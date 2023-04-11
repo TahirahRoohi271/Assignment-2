91be5f3dd05f1a995bf37b6696a3cf501b5763e7
TahirahRoohi271 <130206321+TahirahRoohi271@users.noreply.github.com>
2023-04-11 08:36:50 -0700

    Add files via upload

---
+++ Assignment 2/app.js
@@ -0,0 +1,28 @@
+// Question 3
+
+// function addPara(text) {
+//     let newPara = document.createElement("p");
+//     newPara.textContent = text;
+//     document.body.appendChild(newPara);
+//   }
+  
+//   const addParaBtn = document.getElementById("add-para-btn");
+//   addParaBtn.addEventListener("click", function() {
+//     addPara("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia sequi temporibus dolorem incidunt qui soluta vel molestiae voluptas placeat vero quae ipsum suscipit quidem eligendi tempore distinctio, autem, eveniet similique.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia sequi temporibus dolorem incidunt qui soluta vel molestiae voluptas placeat vero quae ipsum suscipit quidem eligendi tempore distinctio, autem, eveniet similique.");
+//   });
+
+// Question 4
+
+// function addListItem(text) {
+//     const newListItem = document.createElement('li');
+//     newListItem.textContent = text;
+//     const unorderedList = document.querySelector('#myList');
+//     unorderedList.appendChild(newListItem);
+//   }
+ 
+// Question 5
+
+// function backcolor(element, color) {
+//     element.style.backgroundColor = color;
+//   }
+//   let body = document.querySelector("body");
